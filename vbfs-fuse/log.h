6b4f6c22c01d88c903989d2c5ffd3398f6b0ba0c
bianyu <bianyu@kedacom.com>
2013-06-04 09:26:34 -0400

    Reconstruct code in vbfs-fuse 	Something stucks in vbfs-fuse3. Aims: (1) Try to implement a mempool (2) Separate memory data orgization and io-operations (3) Optimize cache design (4) Merge inode into dirent

--- vbfs-fuse/log.h
+++
@@ -1,8 +0,0 @@
-#ifndef __LOG_H_
-#define __LOG_H_
-
-FILE *pLog;
-
-#define log_err(log_file, fmt, args...) do { fprintf(log_file, fmt, ##args); fflush(log_file); } while(0)
-
-#endif
--- vbfs-fuse/super.c
+++
@@ -1,6 +0,0 @@
-#include "fuse_vbfs.h"
-
-int fill_superblock(struct super_block *vbfs_superblock)
-{
-	
-}
